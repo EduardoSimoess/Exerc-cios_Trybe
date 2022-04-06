ecf81416cdc589413fc83e30be94b0f14ffb5df4
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-06 11:44:48 -0300

    Exercício de fixação 5

---
+++ fundamentos/bloco-4-Introdução-ao-JavaScript/aula-1-JavaScript-Primeiros-Passos/fixação5/script.js
@@ -0,0 +1,9 @@
+const weekday = "segunda";
+if (weekday === "segunda" || weekday === "terça" || weekday==="quarta" || weekday === "quinta" || weekday === "sexta")
+{
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else {
+    console.log ("FINALMENTEEE")
+}
+
