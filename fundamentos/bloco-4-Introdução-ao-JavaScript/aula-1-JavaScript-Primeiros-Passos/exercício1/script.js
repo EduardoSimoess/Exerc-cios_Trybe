d88f9f3ac4ec55d5a1e03b99455a92d596b4f3e6
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-06 15:10:41 -0300

    Adição de exercícios 1 a 3

---
+++ fundamentos/bloco-4-Introdução-ao-JavaScript/aula-1-JavaScript-Primeiros-Passos/exercício1/script.js
@@ -0,0 +1,33 @@
+//Quesito 1//
+const A = 50;
+const B = 100;
+console.log (A+B);
+console.log (A-B);
+console.log (A*B);
+console.log (A/B);
+console.log (A%B);
+
+//Questão 2//
+if (A>B) {
+    console.log(A);
+}
+else if (B>A) {
+console.log(B);
+}
+else {
+    console.log("Os valores são iguais")
+}
+//Questão 3//
+const C = 130;
+if (A>B && A>C) {
+    console.log(A);
+}
+else if (B>A && B>C) {
+console.log(B);
+}
+else if (C>A && C>B) {
+console.log(C);   
+}
+else {
+    console.log("Os valores são iguais")
+}
