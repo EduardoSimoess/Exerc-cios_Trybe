0e5b4a88974ffad78c0c4faff9eceab03e33b4fc
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-07 09:04:53 -0300

    Exercício de fixação 1

---
+++ fundamentos/bloco-4-Introdução-ao-JavaScript/aula-2-Array-e-loop-for/fixação/script.js
@@ -0,0 +1,16 @@
+// Questão 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Questão 2
+let indexOfPortfolio = menu.indexOf ("Portfólio");
+
+console.log(indexOfPortfolio);
+
+// Questão 3
+menu.push ("Contato");
+
+console.log (menu);
+
