45009c6cc7dc395962bc7daa7661e3c8d13f3449
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-12 11:11:03 -0300

    exercício de fixação 1

---
+++ fundamentos/bloco-5-DOM-eventos-Web-storage/aula-1-DOM-e-seletores/fixação1/script.js
@@ -0,0 +1,13 @@
+// Fixação 1
+document.querySelector("header").style.backgroundColor = "#2fc18c";
+document.querySelector("section").style.backgroundColor = "lightpink";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+document.querySelectorAll("section")[1].style.backgroundColor = "yellow";
+
+
+
+
+
