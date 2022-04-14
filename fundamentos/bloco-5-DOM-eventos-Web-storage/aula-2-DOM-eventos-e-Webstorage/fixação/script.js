4f5a9cb58d40c9db5e4354b508df9601488ad020
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-14 15:45:13 -0300

    Fixação 1

---
+++ fundamentos/bloco-5-DOM-eventos-Web-storage/aula-2-DOM-eventos-e-Webstorage/fixação/script.js
@@ -0,0 +1,24 @@
+// Fixação
+// Quesito 1
+document.getElementById ("elementoOndeVoceEsta");
+
+// Quesito 2
+document.getElementById ("elementoOndeVoceEsta").parentElement.style.color = "red";
+
+// Quesito 3
+document.getElementById ("elementoOndeVoceEsta").firstElementChild.innerText = "Boa flhão!!";
+
+// Quesito 4
+document.getElementById ("pai").firstChild;
+
+// Questão 5
+document.getElementById ("elementoOndeVoceEsta").previousElementSibling;
+
+// Questão 6
+document.getElementById ("elementoOndeVoceEsta").nextSibling;
+
+// Questão 7
+document.getElementById ("elementoOndeVoceEsta").nextElementSibling;
+
+// Questão 8
+document.getElementById ("pai").lastChild.previousElementSibling;
