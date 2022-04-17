c888255230b1b5144f851d3e6930f778995df194
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-17 15:14:01 -0300

    Quesitos 7 a 9

--- fundamentos/bloco-5-DOM-eventos-Web-storage/aula-2-DOM-eventos-e-Webstorage/exercício/script.js
+++ fundamentos/bloco-5-DOM-eventos-Web-storage/aula-2-DOM-eventos-e-Webstorage/exercício/script.js
@@ -1,6 +1,6 @@
 // Quesito 1
 let body = document.getElementsByTagName('body')[0];
-let title1 = document.createElement ('h1');
+let title1 = document.createElement('h1');
 title1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(title1);
 
@@ -28,3 +28,38 @@ main.appendChild(section2);
 let section3 = document.createElement('section');
 section3.className = 'right-content';
 main.appendChild(section3);
+
+// Quesito 7
+let image = document.createElement('img');
+image.className = "small-image";
+image.src = "https://picsum.photos/200";
+section2.appendChild(image);
+
+// Quesito 8
+let unorderedList = document.createElement('ul');
+section3.appendChild(unorderedList);
+
+let listItens = [
+    'unix',
+    'Git',
+    'HTML',
+    'Lógica de programação',
+    'DOM',
+    'Forms, FlexBox',
+    'JavaScript ES6',
+    'CSS',
+    'Higher Order',
+    'Testes assincrons',
+];
+for (let index = 0; index < listItens.length; index += 1){
+    let listItem = document.createElement('li');
+    listItem.innerText = listItens[index];
+    unorderedList.appendChild(listItem);
+}
+
+// Quesito 9
+for (index = 0; index < 3; index += 1){
+    let h3 = document.createElement('h3');
+    h3.innerText = "h3 - title";
+    main.appendChild(h3);
+}
