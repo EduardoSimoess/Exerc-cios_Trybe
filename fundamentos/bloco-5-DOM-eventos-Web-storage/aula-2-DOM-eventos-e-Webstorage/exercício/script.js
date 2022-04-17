79efd328d5778bc06715d823ed22787384814196
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-04-17 14:30:10 -0300

    Quesitos 1 e 2

---
+++ fundamentos/bloco-5-DOM-eventos-Web-storage/aula-2-DOM-eventos-e-Webstorage/exercício/script.js
@@ -0,0 +1,10 @@
+// Quesito 1
+let body = document.getElementsByTagName('body')[0];
+let title1 = document.createElement ('h1');
+title1.innerText = 'Exercício 5.2 - JavaScript DOM';
+body.appendChild(title1);
+
+// Quesito 2
+let main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
