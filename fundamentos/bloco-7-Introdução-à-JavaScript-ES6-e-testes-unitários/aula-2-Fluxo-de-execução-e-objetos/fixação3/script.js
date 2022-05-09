7ff1fb24af4382f5bf134cc247176581b22e75fa
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-09 15:13:49 -0300

    fixação 3 comentado

--- fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-testes-unitários/aula-2-Fluxo-de-execução-e-objetos/fixação3/script.js
+++ fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-testes-unitários/aula-2-Fluxo-de-execução-e-objetos/fixação3/script.js
@@ -1,28 +1,28 @@
-const person = {
-    name: 'Roberto',
-  };
+// const person = {
+//     name: 'Roberto',
+//   };
   
-  const lastName = {
-    lastName: 'Silva',
-  };
+//   const lastName = {
+//     lastName: 'Silva',
+//   };
   
-  const clone = Object.assign(person, lastName);
+//   const clone = Object.assign(person, lastName);
   
-  console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
-  console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+//   console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+//   console.log(person); // { name: 'Roberto', lastName: 'Silva' }
 
-  clone.name = 'Maria';
+//   clone.name = 'Maria';
 
-console.log('Mudando a propriedade name através do objeto clone')
-console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
-console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
-console.log('--------------');
+// console.log('Mudando a propriedade name através do objeto clone')
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log('--------------');
 
-person.lastName = 'Ferreira';
+// person.lastName = 'Ferreira';
 
-console.log('Mudando a propriedade lastName através do objeto person');
-console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
-console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+// console.log('Mudando a propriedade lastName através do objeto person');
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
 
 
 const person = {
